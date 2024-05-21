d060c5f079789ff7561dca5d1c42883a25560d73
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-05-21 18:55:55 +0600

    popular items from menu section header

---
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SEctionTitle from '../../../components/SectionTitle/SEctionTitle';
+
+const PopularMenu = () => {
+    return (
+        <div>
+            <SEctionTitle subHeading={"Popular Items"} heading={"From Out Menu"}></SEctionTitle>
+        </div>
+    );
+};
+
+export default PopularMenu;
