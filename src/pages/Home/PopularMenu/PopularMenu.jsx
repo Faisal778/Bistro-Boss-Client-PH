dddf324f6d0578b9bdb7d6a4c222fb9b455726f1
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-05-21 19:07:48 +0600

    data fetched from local menu.json using fetch and useEffect

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SEctionTitle from '../../../components/SectionTitle/SEctionTitle';
 
 const PopularMenu = () => {
+
+    const [menu, setMenu] = useState([])
+
+    useEffect(()=>{
+        fetch("menu.json")
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }, [])
     return (
         <div>
             <SEctionTitle subHeading={"Popular Items"} heading={"From Out Menu"}></SEctionTitle>
