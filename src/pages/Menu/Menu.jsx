90c79dd4c1339329e3beedaa9d524e1e6bf75a01
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-05-22 13:03:02 +0600

    data filtered based on desserts, piza, soup,... from menu that got returned from useMenu hook

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Helmet } from "react-helmet-async";
 import Cover from "../shared/cover/Cover";
 import menuImg from "../../assets/menu/banner3.jpg"
 
 const Menu = () => {
+    const [menu] = useMenu();
+    const desserts = menu.filter(item => item.category === 'dessert')
+    const soup = menu.filter(item => item.category === 'soup')
+    const pizza = menu.filter(item => item.category === 'pizza')
+    const salad = menu.filter(item => item.category === 'salad')
+    const offered = menu.filter (item => item.category === 'offered')
   return (
     <div>
       <Helmet>
@@ -11,7 +17,7 @@ const Menu = () => {
       </Helmet>
 
       <Cover image={menuImg} title={"Our Menu"}></Cover>
-      
+
       
     </div>
   );
