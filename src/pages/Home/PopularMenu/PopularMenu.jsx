b8fc8fcc7d7df9af07973269ceb1d0fa0eb0dd1c
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-05-22 12:51:20 +0600

    used custom hook to fetch data in PopularMenu. then filtered data based on category, set that in poopular. maped popular to show menu items

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,27 +1,33 @@
 import React, { useEffect, useState } from "react";
 import SEctionTitle from "../../../components/SectionTitle/SEctionTitle";
 import MenuItem from "../../shared/MenuItem/MenuItem";
+import useMenu from "../../../hooks/useMenu";
 
 const PopularMenu = () => {
-  const [menu, setMenu] = useState([]);
+  const [menu] = useMenu();
+  const popular = menu.filter((item) => item.category === "popular");
+  //   const [menu, setMenu] = useState([]);
 
-  useEffect(() => {
-    fetch("menu.json")
-      .then((res) => res.json())
-      .then((data) => {
-        const popularItems = data.filter((item) => item.category === "popular");
-        setMenu(popularItems);
-        console.log(popularItems)
-      });
-  }, []);
+  //   useEffect(() => {
+  //     fetch("menu.json")
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         const popularItems = data.filter((item) => item.category === "popular");
+  //         setMenu(popularItems);
+  //         console.log(popularItems)
+  //       });
+  //   }, []);
   return (
     <section className="mb-12">
       <SEctionTitle subHeading={"Popular Items"} heading={"From Out Menu"}></SEctionTitle>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {
-            menu.map(item => (
-                <MenuItem key = {item._id} item = {item}></MenuItem>
-            ))
+          popular.map((item) => (
+            <MenuItem key={item._id} item={item}></MenuItem>
+          ))
+          // menu.map(item => (
+          //     <MenuItem key = {item._id} item = {item}></MenuItem>
+          // ))
         }
       </div>
     </section>
