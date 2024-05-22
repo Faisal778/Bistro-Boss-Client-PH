f5ae2df049c8b35f0692fa213827be5ee45f0dd3
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-05-22 12:21:37 +0600

    custom hook useMenu created

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Helmet } from "react-helmet-async";
 import Cover from "../shared/cover/Cover";
 import menuImg from "../../assets/menu/banner3.jpg"
-import PopularMenu from "../Home/PopularMenu/PopularMenu";
+
 const Menu = () => {
   return (
     <div>
@@ -11,11 +11,8 @@ const Menu = () => {
       </Helmet>
 
       <Cover image={menuImg} title={"Our Menu"}></Cover>
-      <PopularMenu></PopularMenu>
-      <Cover image={menuImg} title={"Our Menu"}></Cover>
-      <PopularMenu></PopularMenu>
-      <Cover image={menuImg} title={"Our Menu"}></Cover>
-      <PopularMenu></PopularMenu>
+      
+      
     </div>
   );
 };
