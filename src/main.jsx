8a87ac38af1a8deb9e5b6d9c0f5a487e76650ff4
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-05-25 03:03:48 +0600

    sing in form make sure to use children prop inside provider, else all the pages will be white blank.
    const unsubscribe = onAuthStateChange inside useEffect as it call outsider api
        setUser(user)
        setLoading(false) once user laoded loading will be false
    return () => { retunr unsubscribe()}
    
    //set laoding to true in all below funtions and const before function
    createUser(email, pass)
        return createuserWithEmailAndPassword(auth, email, password)
    
    singIn(email, pass)
        retunr signInWithEmailAndPassword(auth, email, password)
    
    logOut () => {
        retunr signOut(auth)

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,17 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 
 import { router } from "./routes/Router.jsx";
-import {HelmetProvider } from "react-helmet-async";
-
+import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./Providers/AuthProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-    <div className="max-w-[95%] mx-auto">
-      <RouterProvider router={router} />
-    </div>
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <div className="max-w-[95%] mx-auto">
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
