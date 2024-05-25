f2505c0601f22c72f5f9b386d5d9bebd5a1657ff
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-05-25 13:52:01 +0600

    form validation react-hook-form Regex to validate password strength errors.password?.type === 'pattern' && <p role=alert className=text-red-600>                   Password must one upper case, one lower case, one special chanracter                 </p>

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,6 +1,14 @@
 import React from "react";
-
+import { useForm } from "react-hook-form";
 const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -12,24 +20,60 @@ const SignUp = () => {
           </p>
         </div>
         <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form className="card-body">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Name</span>
               </label>
-              <input type="text" placeholder="name" name ="name" className="input input-bordered" required />
+              <input type="text" placeholder="name" {...register("name", { required: true })} name="name" className="input input-bordered" />
+              {errors.name && <span className="text-red-600">name is required</span>}
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
-              <input type="email" placeholder="email" name = "email" className="input input-bordered" required />
+              <input type="email" placeholder="email" {...register("email", { required: true })} name="email" className="input input-bordered" />
+              {errors.email && <span className="text-red-600"> email is required</span>}
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
-              <input type="password" placeholder="password" name = "password" className="input input-bordered" required />
+              <input
+                type="password"
+                {...register("password", {
+                  required: true,
+                  minLength: 6,
+                  maxLength: 20,
+                  pattern: /^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=..*[a-z]).{8}$/,
+                })}
+                placeholder="password"
+                name="password"
+                className="input input-bordered"
+              />
+
+              {errors.password?.type === "required" && (
+                <p role="alert" className="text-red-600">
+                  Password is required
+                </p>
+              )}
+              {errors.password?.type === "minLength" && (
+                <p role="alert" className="text-red-600">
+                  Password must have minimum length of 6
+                </p>
+              )}
+              {errors.password?.type === "maxLength" && (
+                <p role="alert" className="text-red-600">
+                  Password must have maximum length of 20
+                </p>
+              )}
+              {errors.password?.type === "pattern" && (
+                <p role="alert" className="text-red-600">
+                  Password must one upper case, one lower case, one special chanracter
+                </p>
+              )}
+
+              {/* {errors.password && <span className="text-red-600">Password is required</span>}*/}
               <label className="label">
                 <a href="#" className="label-text-alt link link-hover">
                   Forgot password?
