b9905d4705581fa2a7c30805b967554b9cb5810c
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-05-25 16:37:58 +0600

    loading , if user already logged in
    sometimes private route leads to signin page even though user is logged in.
    to solve this issue,
        desturcture loading as well with user
        const {user, loading} = useContext(AuthContext)
        if (loading)
             return <some kind of loading animation>
        if (user)
            return children
    
        return <Navigate to = /login>

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -3,12 +3,15 @@ import { AuthContext } from "../Providers/AuthProvider";
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
-  const { user } = useContext(AuthContext);
+  const { user , loading} = useContext(AuthContext);
 
+  if (loading){
+    return <span className="loading loading-ball loading-lg"></span>
+  }
   if (user) {
     return children;
   }
-  return <Navigate to="/login"></Navigate>;
+  return <Navigate to="/login"/>;
 };
 
 export default PrivateRoute;
